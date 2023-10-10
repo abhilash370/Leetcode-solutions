2da6ace7270aa67cf6af2237978d46948161788e
abhilash370 <91712069+abhilash370@users.noreply.github.com>
2023-10-10 12:47:36 +0530

    Add files via upload

---
+++ delete_nth_node.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* start= new ListNode();
+        start->next=head;
+        ListNode* f=start;
+        ListNode* s=start;
+
+        for(int i=0;i<=n;++i){
+            f=f->next;
+        }
+
+        while(f!=nullptr){
+            f=f->next;
+            s=s->next;
+        }
+        s->next=s->next->next;
+        return start->next;
+
+    }
+};
